69958dcaff08f6c05c236a34ff1390106bfc1b2b
Spencer Tall <tall.spencer@gmail.com>
2019-11-18 12:21:08 -0700

    Added some init and new util file

--- utils/utilitiesCharacter.py
+++ utils/utilitiesCharacter.py
@@ -26,3 +26,31 @@ class BaseStats:
     @classmethod
     def default(cls):
         return cls(0, 10, 10, 10, 10, 10, 10)
+
+    def get_mod(self, stat: str):
+        attr_short = stat.lower()[:3]
+        if attr_short not in ATTRIBUTE_ABBREVIATIONS:
+            raise ValueError(f"{stat} is not a valid stat.")
+        return {
+            'str': self.strength // 2 - 5, 'dex': self.dexterity // 2 - 5,
+            'con': self.constitution // 2 - 5, 'int': self.intelligence // 2 - 5,
+            'wis': self.wisdom // 2 - 5, 'cha': self.charisma // 2 - 5
+        }[attr_short]
+    def __str__(self):
+        return f"**STR**: {self.strength} ({(self.strength - 10) // 2:+}) " \
+               f"**DEX**: {self.dexterity} ({(self.dexterity - 10) // 2:+}) " \
+               f"**CON**: {self.constitution} ({(self.constitution - 10) // 2:+})\n" \
+               f"**INT**: {self.intelligence} ({(self.intelligence - 10) // 2:+}) " \
+               f"**WIS**: {self.wisdom} ({(self.wisdom - 10) // 2:+}) " \
+               f"**CHA**: {self.charisma} ({(self.charisma - 10) // 2:+})"
+    def __getitem__(self, item):  # Error Catching
+        if item not in ATTRIBUTE_NAMES:
+            raise ValueError(f"{item} is not a stat.")
+        return getattr(self, item)
+    
+class BaseLevels:
+    def __init__(self, classes: dict = None, total_level: int = None):
+        if classes is None:
+            classes = {}
+        self.total_level = total_level or sum(classes.values())
+        self.classes = classes
