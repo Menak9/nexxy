62e70f3838e8620ebef3ba5d65337154c2d6cb7c
Spencer Tall <tall.spencer@gmail.com>
2019-11-18 12:46:44 -0700

    Adding classes

---
+++ utils/utilitiesClasses.py
@@ -0,0 +1,10 @@
+CLASSES = (Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Wizard, 
+Alchemist, Cavalier, Gunslinger, Inquisitor, Magus, Omdura, Oracle, Shifter, Summoner, Witch, Vampire Hunter, 
+Vigilante, Arcanist, Bloodrager, Brawler, Hunter, Investigator, Shaman, Skald, Slayer, Swashbuckler, Warpriest
+Kineticist, Medium, Mesmerist, Occultist, Psychic, Spiritualist, Antipaladin, Ninja, Samurai, Adept,
+Aristocrat, Commoner, Expert, Warrior)
+PRESTIGE_CLASSES = (Battle Herald, Holy Vindicator, Horizon Walker, Master Chymist, Master Spy,
+Nature Warden, Rage Prophet, Stalwart Defender, Arcane Archer, Arcane Trickster, Assassin, Chronicler,
+Dragon Disciple, Duelist, Eldritch Knight, Loremaster, Mystic THeurge, Shadowdancer, Anchorite of Dawn,
+Argent Dramaturge, Asavir, Ashavic Dancer, Brewkeeper, Agent of the Grave, Arcane Savant, Bloatmage,
+Brightness Seeker, Brother of the Seal,)
