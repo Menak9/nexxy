55032c8dc5cae64d920d401f7815adf5309ba80b
Spencer Tall <tall.spencer@gmail.com>
2019-11-18 18:12:08 -0700

    Update Classes

--- utils/utilitiesClasses.py
+++ utils/utilitiesClasses.py
@@ -3,8 +3,30 @@ Alchemist, Cavalier, Gunslinger, Inquisitor, Magus, Omdura, Oracle, Shifter, Sum
 Vigilante, Arcanist, Bloodrager, Brawler, Hunter, Investigator, Shaman, Skald, Slayer, Swashbuckler, Warpriest
 Kineticist, Medium, Mesmerist, Occultist, Psychic, Spiritualist, Antipaladin, Ninja, Samurai, Adept,
 Aristocrat, Commoner, Expert, Warrior)
+
 PRESTIGE_CLASSES = (Battle Herald, Holy Vindicator, Horizon Walker, Master Chymist, Master Spy,
 Nature Warden, Rage Prophet, Stalwart Defender, Arcane Archer, Arcane Trickster, Assassin, Chronicler,
 Dragon Disciple, Duelist, Eldritch Knight, Loremaster, Mystic THeurge, Shadowdancer, Anchorite of Dawn,
 Argent Dramaturge, Asavir, Ashavic Dancer, Brewkeeper, Agent of the Grave, Arcane Savant, Bloatmage,
-Brightness Seeker, Brother of the Seal,)
+Brightness Seeker, Brother of the Seal, Death Slayer, Crimson Templar, Darechaser, Devoted Muse, Celestial Knight, Champion of the Enlightened, 
+Chvalier, Coastal Pirate, Collegiate Arcanist, Crimson Assassin, Cyphermage, Daivrat, Dark Delver,
+Darkfire Adept, Deep Sea Pirate, Demoniac, Diabolist, Dissident of Dawn, Divine Assessor, Divine Scion,
+Esoteric Knight, Forest Warden, Hell Knight Enforcer, Gray Corsair, Guild Devil, Harvester Tiller,
+Heritor Knight, Hinterlander, Enchanting Courtesan, Envoy of Balance, Evangelist, Exalted, Falsse Priest, 
+Field Agent, Furious Guardian, Genie Binder, Golden Legionnaire, Grand Marshal, Gray Warden,
+Green Faith Acolyte, Group Leader, Guild Agent, Guild Poisoner, Halfling Opportunist, Harrower,
+Hell Knight, Hell Knight Signifier, Mortal Usher, Inheritors Crusader, Justiciar, Lantern Bearer, 
+Liberator, Lion Blade, Living Monolith, Low Templar, Mage of the Third Eye, Mammoth Rider, Mystery Cultist,
+Proctor, Ritualist, Rose Warden, Runeguard, Natural Alchemist, Noble Scion, Pain Taster, Pit Fighter, 
+Planes Walker, Purity Legion Enforcer, Sanguine Angel, Stargazer, Souldrinker, Spirit Channeler Emissary,
+Storm Kindler, Student of Perfection, Twilight Talon, Sacred Sentinel, Scar Seeker, Sanctified Prophet, Sentinel,
+Sleepless Detective, Soul Warden, Souldrinker2, Sphere Singer, Spherewalker, Steel Falcon, Student of War,
+Sun Seeker, Swordlord, Tattooed Mystic, Technomancer, Umbral Agent, Veiled Illusionist, Winter Witch)
+
+BARBARIAN_ARCHETYPES = (Armored Hulk, Breaker, Brutal Pugilist, Burn Rider, Dreadnought, Drunken Brute,
+Drunken Rager, Elemental Kin, Fearsome Defender, Flesheater, Geminate Invoker, Hurler, Invulnerable Rager,
+Jungle Rager, Liberator, Mad Dog, Mooncursed, Mounted Fury, Pack Rager, Primal Hunter, Raging Cannibal,
+Savage Barbarian, Savage Tattooist, Scarred Rager, Sea Reaver, Superstitious, Titan Mauler,
+Totem Warrior, True Primitive, Untamed Rager, Urban Barbarian, Wild Rager)
+
+BARD_ARCHETYPES = ()
