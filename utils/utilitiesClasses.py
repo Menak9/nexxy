3a3cb5a9d221d9b6d299d8208ec16d9d32dec06c
Spencer Tall <tall.spencer@gmail.com>
2019-11-19 00:52:57 -0700

    Update to Classes

--- utils/utilitiesClasses.py
+++ utils/utilitiesClasses.py
@@ -27,6 +27,52 @@ BARBARIAN_ARCHETYPES = (Armored Hulk, Breaker, Brutal Pugilist, Burn Rider, Drea
 Drunken Rager, Elemental Kin, Fearsome Defender, Flesheater, Geminate Invoker, Hurler, Invulnerable Rager,
 Jungle Rager, Liberator, Mad Dog, Mooncursed, Mounted Fury, Pack Rager, Primal Hunter, Raging Cannibal,
 Savage Barbarian, Savage Tattooist, Scarred Rager, Sea Reaver, Superstitious, Titan Mauler,
-Totem Warrior, True Primitive, Untamed Rager, Urban Barbarian, Wild Rager)
+Totem Warrior, Truee Primitive, Untamed Rager, Urban Barbarian, Wild Rager, Feral Gnasher, Hateful Rager)
 
-BARD_ARCHETYPES = ()
+BARD_ARCHETYPES = (Animal Speaker, Arbiter, Arcane Duelist, Arcane Healer, Archeologist, Archivist, 
+Argent Voice, Arrowsong Minstrel, Brazen Deceiver, Buccaneer, Busker, Celebrity, Court Bard, Court Fool,
+Daredevil, Demagogue, Dervish Dancer, Dervish of Dawn, Detective, Dirge Bard, Diva, Duettist, Faith Singer, Flame Dancer,
+Geisha, Hoaxer, Impervious Messenger, Juggler, Lotus Geisha, Magician, Masked Performer, Mute Musician, Negotiator,
+Phrenologist, Provocatuer, Ringleader, Sandman, Savage Skald, Sea Singer, Silver Balladeer, Solacer,
+Songhealer, Sorrowsoul, Sound Striker, Street Performer, Studious Librarian, THundercaller, Voice of the Wild, 
+Wilt, Dragon Herald, Dragon Yapper, Prankster, Shadow Puppeteer, Watersinger)
+
+CLERIC_ARCHETYPES = (Angelfire Apostle, Appeaser, Asmodean Advocate, Blossoming Light, Cardinal, Cloistered Cleric,
+Crusader, Crashing Wave Cleric, Devilbane Priest, Devoit Pilgrim, Divine Paragon, Divine Scourge, 
+Divine Strategist, Ecclesitheurge, Elder Mythos Cultist, Evangelist, Foundation of Faith, Herald Caller, 
+Hidden Priest, Iron Priest, Merciful Healer, Roaming Exorcist, Sacred Attendant, Scroll Scholar, Separatist,
+Stoic Caregiver, Theologian, Undead Lord, Fiendish Vessel, Demonic Apostle, Forgemaster)
+
+DRUID_ARCHETYPES = (Ancient Guardian, Draconic Druid, Elemental Ally, Feral Shifter, Goliath Druid, 
+Green Faith Initiate, Halcyon Druid, Kraken Caller, Leshy Warden, Life Channeler, Menhir Savant, Mooncaller,
+Nature Fang, Pack Lord, Reincarnated Druid, Restorer, Skinshaper, Storm Druid, Sunrider, Supernaturalist,
+Survivor Druid, Swarm Monger, Toxicologist, Urushiol Druid, Veiled Adept, Wild Whisperer, Aquatic Druid, 
+Arctic Druid, Blight Druid, Cave Druid, Desert Druid, Jungle Druid, Mountain Druid, Plains Druid, 
+River Druid, Swamp Druid, Tempest Druid, Urban Druid, Ape Shaman, Bat Shaman, Bear Shaman, Boar Shaman, 
+Dragon Shaman, Eagle Shaman, Lion Shaman, Saurian Shaman, Serpent Shaman, Shark Shaman, Wolf Shaman, 
+Feral Child, Naga Aspirant, Season Keeper, Sky Druid, Treesinger, Troll Fury, Undine Adept)
+
+FIGHTER_ARCHETYPES = (Archer, Armiger, Armor Master, Aquanaut, Blackjack, Border Defender, Brawler, 
+Buckler Duelist, Cad, Child of War, Corsair, Crossbowman, Cyber-Soldier, Defender, Dervish of Dawn, Dragoon,
+Dragonheir Scion, Drill Sergeant, Eldritch Guardian, Free Hand Fighter, Free-Style Fighter, Gladiator, 
+High Guardian, Learned Duelist, Lore Warden, Lore Warden2, Martial Master, Mobile Fighter, Mutation Warrior, 
+Opportunist Fighter, Pack Mule, Phalanx Soldier, Polearm Master, Relic Master, Roughrider, Savage Warrior, 
+Seasoned Commander, Sensate, Shielded Fighter, Siegebreaker, Skirmisher, Swordlord, Tactician, Thunderstriker, 
+Titan Fighter, Tower Shield Specialist, Trench Fighter, Two-Handed Fighter, Two-Weapon Warrior, Unarmed Fighter,
+Unbreakable, Vengeful Hunter, Viking, Weapon Bearer Squire, Weapon Master, Airborne Ambusher, Cavern Sniper,
+Foehammer, Swarm Fighter)
+
+MONK_ARCHETYPES = (Black Asp, Brazen Disciple, Disciple of Wholeness, Drunken Master, Far Strike Monk, 
+Flowing Monk, Hamatulatsu Master, Harrow Warden, Hungry Ghost Monk, Invested Regent, Karmic Monk, Kata Master, 
+Ki Mystic, Maneuver Master, Martial Artist, Master of Many Styles, Monk of the Empty Hand, Monk of the Four Winds,
+Monk of the Healing Hand, Monk of the Iron Mountain, Monk of the Lotus, Monk of the Mantis, Monk of the Seven Winds,
+Perfect Scholar, Qinggong Monk, Sage Counselor, Scaled Fist, Scarred Monk, Sensei, Serpent Fire Adept, Sohei, Spirit Master, 
+Terra-Cotta Monk, Tetori, Weapon Adept, Wildcat, Windstep Master, Zen Archer, Contemplative, Gray Disciple, Ironskin Monk,
+Nimble Guardian, Student of the Stone, Treetop Monk, Underfoot Adept, Wanderer)
+
+PALADIN_ARCHETYPES = (Chosen One, Combat Healer Squire, Divine Defender, Divine Guardian, Divine Hunter, Dusk Knight, Empyreal Knight, Enforcer, Enlightened Paladin,
+Ghost Hunter, Gray Paladin, Holy Guide, Holy Gun, Holy Tactician, Hospitaler, Legate, Martyr, Mind Sword, Oathbound Paladin, Pearl Seeker,
+Sacred Servant, Sacred Shield, Scion of Peace, Shining Knight, SIlver Champion, Soul Sentinel, Sword of Valor, Tempered Champion, Temple Champion, 
+Tortured Crusader, Undead Scourge, Virtuous Bravo, Warrior of the Holy Light, Redeemer, Stonelord, Tranquil Guardian)
+
+RANGER_ARCHETYPES = ()
